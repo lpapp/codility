1328305930bc7af5316d00b29b67d5953fb91031
Laszlo Papp <laszlo.papp@foundry.com>
2021-10-31 13:30:43 +0000

    Add brackets

---
+++ lesson7/brackets.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int solution(string &S)
+{
+  stack<char> st;
+
+  for (const auto c : S) {
+    if (c == '}' && st.top() == '{') st.pop();
+    else if (c == ']' && st.top() == '[') st.pop();
+    else if (c == ')' && st.top() == '(') st.pop();
+    else st.push(c);
+  }
+
+  return st.empty() ? 1 : 0;
+}
+
+int main()
+{
+  string i1 = "{[()()]}";
+  cout << "{[()()]} => 1: " << solution(i1) << endl;
+
+  string i2 = "([)()]";
+  cout << "([)()] => 0: " << solution(i2) << endl;
+
+  string i3 = "()[()]";
+  cout << "()[()] => 1: " << solution(i3) << endl;
+
+  string i4 = "{({})}[]()";
+  cout << "{()}() => 1: " << solution(i4) << endl;
+
+  string i5 = "{({})}[]()";
+  cout << "{()}() => 1: " << solution(i5) << endl;
+
+  return 0;
+}
