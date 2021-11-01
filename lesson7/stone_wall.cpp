d94ff01350dd39ee72acc5bea6686d9e9af7877b
Laszlo Papp <laszlo.papp@foundry.com>
2021-11-01 19:58:41 +0000

    Use stack for stone_wall

--- lesson7/stone_wall.cpp
+++ lesson7/stone_wall.cpp
@@ -1,43 +1,27 @@
 #include <iostream>
-#include <queue>
+#include <stack>
 #include <vector>
-
-// Basic theory: minimum number of blocks equals to the different sizes.
-// Same disjointed slices count +1
-// Slices with same edges count -1
-// So, we can spare blocks in two cases:
-// 1. First and last of a slice are the same height
-// 2. Consecutive blocks are the same height.
-
-// 1. Go through equal heights, make them 1 block.
-// 2. 
 
 using namespace std;
 
 int solution(vector<int> &H)
 {
   size_t sizeH = H.size();
-  queue<int> q;
+  stack<int> s;
   int min_block = 0;
   for (size_t i = 0; i < sizeH; ++i) {
     int e = H[i];
-    if (q.empty() || e > q.front()) q.push(e);
-    else if (e < q.front()) {
-      q.pop();
-      ++min_block;
-      if (!q.empty() && q.front() == q.back()) {
-        min_block += q.size() - 1;
-        while (!q.empty()) q.pop();
+    if (s.empty() || e > s.top()) s.push(e);
+    else {
+      while (!s.empty() && e <= s.top()) {
+        if (e < s.top()) ++min_block;
+        s.pop();
       }
-      q.push(e);
-    }
-    else if (e == q.front()) {
-      min_block += q.size();
-      while (!q.empty()) q.pop();
+      s.push(e);
     }
   }
 
-  min_block += q.size();
+  min_block += s.size();
   return min_block;
 }
 
@@ -46,11 +30,28 @@ int main()
   vector<int> i1{8, 8, 5, 7, 9, 8, 7, 4, 8};
   cout << "{8, 8, 5, 7, 9, 8, 7, 4, 8} => 7: " << solution(i1) << endl;
 
+  //   X
+  // X X X
+  // X X X X
   vector<int> i2{2, 3, 2, 1};
   cout << "{2, 3, 2, 1} => 3: " << solution(i2) << endl;
 
   vector<int> i3{1, 1, 1};
   cout << "{1, 1, 1} => 1: " << solution(i3) << endl;
+
+  //     X X
+  //   X X X X
+  // X X X X X X
+  vector<int> i4{1, 2, 3, 3, 2, 1};
+  cout << "{1, 2, 3, 3, 2, 1} => 3: " << solution(i4) << endl;
+
+  //           X
+  //       X   X
+  //     X X X X
+  //   X X X X X
+  // X X X X X X
+  vector<int> i5{1, 2, 3, 4, 3, 5};
+  cout << "{1, 2, 3, 4, 3, 5} => 5: " << solution(i5) << endl;
 
   return 0;
 }
