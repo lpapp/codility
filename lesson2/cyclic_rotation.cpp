0d661baf230bb996b13d29325cc81ab3808ce0a8
Laszlo Papp <laszlo.papp@foundry.com>
2021-11-03 22:28:14 +0000

    Reserve in the cyclic rotation

--- lesson2/cyclic_rotation.cpp
+++ lesson2/cyclic_rotation.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> solution(std::vector<int> &A, int K)
+using namespace std;
+
+vector<int> solution(vector<int> &A, int K)
 {
-  uint8_t size_A = A.size();
-  if (!size_A) {
-    return std::vector<int>{};
+  uint8_t sizeA = A.size();
+  if (!sizeA) {
+    return vector<int>{};
   }
 
   // This could overflow, so count the modulo. Shifting more than the size of
   // array is equal to shifting - size of array, etc.
-  uint8_t shift = K % size_A;
+  uint8_t shift = K % sizeA;
 
 
-  std::vector<int> result;
+  vector<int> result;
+  result.reserve(sizeA);
   // Cycle what needs cycling from the right side to the left.
   for (size_t i = 0; i < shift; ++i) {
-    result.push_back(A[size_A - shift + i]);
+    result.push_back(A[sizeA - shift + i]);
   }
 
   // Shift the beginning of the array towards the right without overflow.
-  for (size_t i = 0; i < size_A - shift; ++i) {
+  for (size_t i = 0; i < sizeA - shift; ++i) {
     result.push_back(A[i]);
   }
 
@@ -33,46 +36,46 @@ int main()
   // 1, 2, 3, 4 => (2) => 3 4 1 2
   // 1, 2, 3, 4 => (3) => 2 3 4 1
 
-  std::vector<int> input1{3, 8, 9, 7, 6};
+  vector<int> input1{3, 8, 9, 7, 6};
   auto result1 = solution(input1, 3); 
-  std::cout << "[3, 8, 9, 7, 6] (3) => [9, 7, 6, 3, 8]: ";
+  cout << "[3, 8, 9, 7, 6] (3) => [9, 7, 6, 3, 8]: ";
   for (const auto i : result1) {
-    std::cout << i << " ";
+    cout << i << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
 
-  std::vector<int> input2{3, 8, 9, 7, 6};
+  vector<int> input2{3, 8, 9, 7, 6};
   auto result2 = solution(input2, 1); 
-  std::cout << "[3, 8, 9, 7, 6] (1) => [6, 3, 8, 9, 7]: ";
+  cout << "[3, 8, 9, 7, 6] (1) => [6, 3, 8, 9, 7]: ";
   for (const auto i : result2) {
-    std::cout << i << " ";
+    cout << i << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
 
-  std::vector<int> input3{0, 0, 0};
+  vector<int> input3{0, 0, 0};
   auto result3 = solution(input3, 1); 
-  std::cout << "[0, 0, 0] (1) => [0, 0, 0]: ";
+  cout << "[0, 0, 0] (1) => [0, 0, 0]: ";
   for (const auto i : result3) {
-    std::cout << i << " ";
+    cout << i << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
 
 
-  std::vector<int> input4{1, 2, 3, 4};
+  vector<int> input4{1, 2, 3, 4};
   auto result4 = solution(input4, 4); 
-  std::cout << "[1, 2, 3, 4] (4) => [1, 2, 3, 4]: ";
+  cout << "[1, 2, 3, 4] (4) => [1, 2, 3, 4]: ";
   for (const auto i : result4) {
-    std::cout << i << " ";
+    cout << i << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
 
-  std::vector<int> input5{};
+  vector<int> input5{};
   auto result5 = solution(input5, 100); 
-  std::cout << "[] (100) => []: ";
+  cout << "[] (100) => []: ";
   for (const auto i : result5) {
-    std::cout << i << " ";
+    cout << i << " ";
   }
-  std::cout << std::endl;
+  cout << endl;
 
   return 0;
 }
